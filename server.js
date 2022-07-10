67d173bb8f90d06fa8247b8935f058ba17c0fbb2
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-07-10 12:17:01 -0400

    created basic get single, delet, and add

--- server.js
+++ server.js
@@ -21,8 +21,36 @@ const db = mysql.createConnection(
   console.log("Connected to the employees database.")
 );
 
-db.query(`SELECT * FROM departments`, (err, rows) => {
-  console.log(rows);
+// GET a single department
+//db.query(`SELECT * FROM departments WHERE id = 1`, (err, row) => {
+//  if (err) {
+//    console.log(err);
+//  }
+//  console.log(row);
+//});
+
+//db.query(`SELECT * FROM departments`, (err, rows) => {
+//  console.log(rows);
+//});
+
+// Delete a department
+//db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+//  if (err) {
+//    console.log(err);
+//  }
+//  console.log(result);
+//});
+
+// Create a department
+//const sql = `INSERT INTO departments (id, name)
+//              VALUES (?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 // Default response for any other request (Not Found)
