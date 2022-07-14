d1c2c0dfa617fc7d4761d5ab80c9d30bc4a57e7b
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-07-13 21:09:56 -0400

    fixed dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
-const mysql = require("mysql2");
-
 require("dotenv").config();
+const mysql = require("mysql2");
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    password: "Gc5yvm2mq!",
+    password: process.env.DB_PASSWORD,
     database: "employees",
   },
   console.log("Connected to the employees database.")
